f9ee75785ec0470a78cbb7d7d7b85f4b741a9a1c
Denys Baliuk <denuk14228@gmail.com>
2026-04-16 23:54:30 +0300

    first try done project movie-moviesession-cinemahall

--- db/models.py
+++ db/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.db.models import ManyToManyField
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -14,3 +16,36 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def capacity(self):
+        return self.rows * self.seats_in_row
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField(auto_now_add=True)
+    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.movie.title} {self.show_time}"
+
